ff914981513dc6a27da24d6a3cd1d7c607cc9812
Ray Deck <ray@raydeck.com>
2021-07-08 19:13:26 -0500

    fix: add additional elements to the test

--- src/bin.tsx
+++ src/bin.tsx
@@ -1,14 +1,21 @@
 #!/usr/bin/env node
 import commander from "commander";
 import { render } from ".";
-import React, { FC } from "react";
+import React, { FC, Fragment } from "react";
 
-const Test: FC = () => {
-  console.log("hello!!!!");
-  return null;
+const Test: FC<{ index: string }> = ({ index, children }) => {
+  console.log("hello!!!!" + index);
+  return <Fragment>{children}</Fragment>;
 };
 
-render(<Test />);
+render(
+  <Test index="A">
+    <Test index="B">
+      <Test index="D" />
+    </Test>
+    <Test index="C" />
+  </Test>
+);
 
 commander.parse(process.argv);
 export { commander };
